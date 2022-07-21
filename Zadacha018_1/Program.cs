f73ba68bf3e4e945fbb773da7f7e8c0deaaa3fcb
Svetlana <brodlik@me.com>
2022-07-21 19:53:05 +0300

    Zadacha 018_1_array

---
+++ Zadacha018_1/Program.cs
@@ -0,0 +1,15 @@
+﻿string [] ranges = 
+{
+    "x>0, y>0",
+    "x<0, y>0",
+    "x<0, y<0",
+    "x>0, y<0",
+};
+Console.WriteLine ("Введите номер четверти: ");
+int quater = Convert.ToInt32(Console.ReadLine());
+if (quater>=0 && quater<=4)
+Console.WriteLine (ranges[quater-1]);
+else
+{
+    Console.WriteLine ("Нет такой четверти");
+}
