8beeca84313e284dcae2b6d9cb25babe3e213baf
Svetlana <brodlik@me.com>
2022-07-21 20:37:46 +0300

    Zadacha 019_polindrom

---
+++ Zadacha019/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+Console.WriteLine ("Введите пятизначное число: ");
+string? polindrom = Console.ReadLine ();
+
+
+if (polindrom.Length==5)
+{
+    if (polindrom[0]==polindrom[4] && polindrom[1]==polindrom[3])
+    {
+        Console.WriteLine($"Число {polindrom} является полиндромом");
+    }
+    else
+    {
+        Console.WriteLine($"Число {polindrom} не является полиндромом");
+    }
+}
+else
+{
+    Console.WriteLine ("Это число не подходит, оно не пятизначное");
+}
